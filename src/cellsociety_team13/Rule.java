b45f43437fc65e8ece0f32c65bd6ce6e521f5cdb
Lucia Martos <lm260@duke.edu>
2016-09-20 16:16:00 -0400

    Added the SpreadingOFire class which extends Rule class and added a new find neighbour method

--- src/cellsociety_team13/Rule.java
+++ src/cellsociety_team13/Rule.java
@@ -1,19 +1,53 @@
 package cellsociety_team13;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class Rule {
 
 	HashMap<String, Integer> states;
-	HashMap<String, Integer> parameters; 
-	
+	HashMap<String, Integer> parameters;
+
 	abstract void evaluateCell(Cell myCell, CellGrid myGrid);
+
+	ArrayList<Cell> getNonDiagNeighbours(Cell myCell, CellGrid myGrid){
+		ArrayList<Cell> nonDiagNeighbours = new ArrayList<Cell>();
+		if (!myGrid.getCell(myCell.getX() + 1, myCell.getY()) == null) {
+			nonDiagNeighbours.add(myGrid.getCell(myCell.getX() + 1, myCell.getY()));
+		}
+		if (!myGrid.getCell(myCell.getX() - 1, myCell.getY()) == null) {
+			nonDiagNeighbours.add(myGrid.getCell(myCell.getX() - 1, myCell.getY()));
+		}
+		if (!myGrid.getCell(myCell.getX(), myCell.getY() + 1) == null) {
+			nonDiagNeighbours.add(myGrid.getCell(myCell.getX(), myCell.getY() + 1));
+		}
+		if (!myGrid.getCell(myCell.getX(), myCell.getY() - 1) == null) {
+			nonDiagNeighbours.add(myGrid.getCell(myCell.getX(), myCell.getY() - 1));
+		}
+		return nonDiagNeighbours;
+	}
 	
-	void getHashMaps(){
+	ArrayList<Cell> getNeighbours(Cell myCell, CellGrid myGrid) {
+		ArrayList<Cell> myNeighbours = new ArrayList<Cell>(getNonDiagNeighbours(myCell, myGrid));
+		if (!myGrid.getCell(myCell.getX() + 1, myCell.getY()+1) == null){
+			myNeighbours.add(myGrid.getCell(myCell.getX() + 1, myCell.getY() +1) == null);
+		}
+		if (!myGrid.getCell(myCell.getX() + 1, myCell.getY()-1) == null){
+			myNeighbours.add(myGrid.getCell(myCell.getX() + 1, myCell.getY() -1) == null);
+		}
+		if (!myGrid.getCell(myCell.getX() -1, myCell.getY()-1) == null){
+			myNeighbours.add(myGrid.getCell(myCell.getX() - 1, myCell.getY() -1) == null);
+		}		
+		if (!myGrid.getCell(myCell.getX() - 1, myCell.getY()+1) == null){
+			myNeighbours.add(myGrid.getCell(myCell.getX() - 1, myCell.getY() +1) == null);
+		}
+		return myNeighbours;
+	}
+	
+
+	void getHashMaps() {
 		states = new HashMap<String, Integer>(myCellGrid.getStates());
 		parameters = new HashMap<String, Integer>(myCellGrid.getParameters());
 	}
 
-
-	
 }
