ab7baad03d12e4018203f83b45b0966f071ab762
Will Long <wpl5@duke.edu>
2016-09-23 16:20:38 -0400

    Refactored CellGrid
    Updated CellGrid for proper interaction with other classes. Now
    extends Group so that it handles front end and back end at the
    same time.

--- src/cellsociety_team13/CellGrid.java
+++ src/cellsociety_team13/CellGrid.java
@@ -1,60 +1,70 @@
 package cellsociety_team13;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javafx.scene.Group;
 import rule.Rule;
 
-//The cell grid class is simply an array of 
-//Cells (obj) which extends Group. CellGrid 
-//interacts with the Rule class by making an instance of it. 
-//The cell grid will be in charge of calling the for the Rule 
-//class to calculate the next state of the cells and then change 
-//the state of the cells.
 
-public class CellGrid {
-	private int width; // Number of columns
-	private int height; // Number of rows
-	private int cellWidth;
-	private int cellHeight;
-	private int x;
-	private int y;
+/**
+ * CellGrid is responsible for holding the current Cells within the Cell Society
+ * game. It stores the position of the cells and uses the specified Rule class
+ * to update the Cells when specified.
+ */
+
+public class CellGrid extends Group {
+	private double drawWidth, drawHeight;
+	private double drawCellWidth, drawCellHeight;
+
+	private int gridWidth, gridHeight;
+	private Cell[][] cells;
 	private Rule rule;
-	private ArrayList<Cell> cells = new ArrayList<Cell>();
-	private boolean run = false;
+	private boolean isRunning;
 	
-	public CellGrid(int width, int height, int cellWidth, int cellHeight, int x, int y, Rule rule, Group root){
-		this.setWidth(width); this.setHeight(height);
-		this.cellWidth = cellWidth; this.cellHeight = cellHeight;
+	public CellGrid(double xPos, double yPos, double drawWidth, double drawHeight,
+					int gridWidth, int gridHeight, String[][] initialCellTypes, Rule rule) {
+		setLayoutX(xPos);
+		setLayoutY(yPos);
+		this.drawWidth = drawWidth;
+		this.drawHeight = drawHeight;
+		drawCellWidth = drawWidth / gridWidth;
+		drawCellHeight = drawHeight / gridHeight;
+
+		this.gridWidth = gridWidth;
+		this.gridHeight = gridHeight;
+		cells = new Cell[gridHeight][gridWidth];
+		for (int r = 0; r < gridHeight; r++) {
+			for (int c = 0; c < gridWidth; c++) {
+				addCell(initialCellTypes[r][c], r, c);
+			}
+		}
+
 		this.rule = rule;
-		this.x = x;
-		this.y = y;
-		for (int row = 0; row < height; row++){
-			for (int col = 0; col < width; col++){
-				// Based on Rule Type Create Different Cells
-				Cell cell = new Cell(col, row, cellWidth, cellHeight, x, y);
-				cells.add(cell);
-				root.getChildren().add(cell.shape);
-			} // End of Width For
-		} // End of Length For
+	}
+
+	private void addCell(String cellType, int row, int col) {
+		double xPos = col * drawCellWidth;
+		double yPos = row * drawCellHeight;
+		Cell cell = new Cell(cellType, xPos, yPos, drawCellWidth, drawCellHeight);
+		getChildren().add(cell);
+		cells[row][col] = cell;
 	}
 	
-	public ArrayList<Cell> getCellsWithState(String myType, CellGrid myGrid){
-		ArrayList<Cell> myCells = new ArrayList<Cell>();
-		
-			for(int i = 0; i < myGrid.getWidth(); i ++){
-				for(int j = 0; j < myGrid.getHeight(); j ++){
-					if(myGrid.getCell(i, j).getCurrentState().equals(myType)){
-						myCells.add(myGrid.getCell(i, j));
+	public List<Cell> getCellsByType(String cellType){
+		List<Cell> myCells = new ArrayList<>();
+			for(int r = 0; r < gridHeight; r ++){
+				for(int c = 0; c < gridWidth; c ++){
+					if (cells[r][c].getCurrentType().equals(cellType)) {
+						myCells.add(cells[r][c]);
 					}
 				}
 			}
 		return myCells;
 	}
 	
-	public Cell getCell(int column, int row){
-		int arrayPos = row*width + column;
-		return cells.get(arrayPos);
+	public Cell getCell(int row, int col){
+		return cells[r][c];
 	}
 	
 	public void step(){
@@ -62,68 +72,10 @@ public class CellGrid {
 	}
 	
 	public void run() throws InterruptedException{
-		this.run = true;
-		while (run){
+		this.isRunning = true;
+		while (isRunning){
 			this.step();
 			Thread.sleep(5000);
 		}
 	}
-
-	
-	public int getWidth() {return width; }
-	public void setWidth(int width) {this.width = width;}
-	public int getHeight() {return height;}
-	public void setHeight(int length) {this.height = length;}
-
-	
-	
-	
-	
-//	// User inputs filename and hits submit button
-//	// Method in CellGrid...
-//	void newGame(String filename) {
-//	    // Assume XMLGameReader parses the file properly
-//	    XMLGameReader gameReader = new XMLGameReader(filename);
-//	    setSize(gameReader.getGameWidth(), gameReader.getGameHeight());
-//	    setRule(gameReader.getRuleName());
-//	    Map<String, Integer> params = gameReader.getInitialParameters();
-//	    for (String name : params.keySet()) {
-//	        getRule().setParameter(name, params.get(name));
-//	    }
-//	    initializeCells(gameReader.getInitialCellStates());
-//	}
-	
-	
-//	// User has adjusted value of parameter probCatch to int val through UI slider
-//	cellGrid.getRule().setParameter("probCatch", value);
-//
-//	// And the relevant method in Rule...
-//	setParameter(String name, int value) {
-//	    parameterMap.put(name, value);
-//	}
-	
-	
-//	// Assume that a game has been properly loaded from XML file
-//	// User triggers update through Next or Run button
-//	cellGrid.update();
-//	// And within CellGrid...
-//	void update() {
-//	    for (Cell cell : cellGrid) {
-//	        rule.evaluate(cell, cellGrid);
-//	    }
-//	    for (Cell cell : cellGrid) {
-//	        cell.updateNextState();
-//	    }
-//	    drawCells();
-//	}
-	
-	
-//	// User has adjusted value of parameter probCatch to int val through UI slider
-//	cellGrid.getRule().setParameter("probCatch", value);
-//
-//	// And the relevant method in Rule...
-//	setParameter(String name, int value) {
-//	    parameterMap.put(name, value);
-//	}
-	
 }
