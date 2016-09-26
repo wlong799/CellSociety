e85132d7817c83ef97c3f359883e145f71e6b99f
Will Long <wpl5@duke.edu>
2016-09-26 03:02:38 -0400

    Extracted XML control elements to own GUI class

--- src/gui/InputPanel.java
+++ src/gui/InputPanel.java
@@ -4,9 +4,7 @@ import cellsociety_team13.CellGrid;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -15,8 +13,7 @@ public class InputPanel extends Group {
     private static final double ARC_SIZE = 50;
 
     private Rectangle background;
-    private TextField xmlFilenameField;
-    private Button submitFileButton;
+    private XMLFileControl xmlFileControl;
     private AnimationControl animationControl;
     private ComboBox parameterAdjustmentBox;
 
@@ -31,31 +28,23 @@ public class InputPanel extends Group {
         background.setArcWidth(ARC_SIZE);
         background.setArcHeight(ARC_SIZE);
 
-        xmlFilenameField = new TextField("XML GAME FILE");
-        xmlFilenameField.setMinWidth(200);
-        xmlFilenameField.setMaxWidth(200);
+        xmlFileControl = new XMLFileControl(0, 0, 200, height, submitFileHandler);
 
-        submitFileButton = new Button("SUBMIT");
-        submitFileButton.setLayoutX(225);
-        submitFileButton.setMinWidth(100);
-        submitFileButton.setMaxWidth(100);
-        submitFileButton.setOnAction(submitFileHandler);
-
-        animationControl = new AnimationControl(350, 0, 100, height, cellGrid);
+        animationControl = new AnimationControl(225, 0, 100, height, cellGrid);
 
         parameterAdjustmentBox = new ComboBox();
-        parameterAdjustmentBox.setLayoutX(500);
-        parameterAdjustmentBox.setMinWidth(width - 500);
-        parameterAdjustmentBox.setMaxWidth(width - 500);
+        parameterAdjustmentBox.setLayoutX(350);
+        parameterAdjustmentBox.setMinWidth(width - 350);
+        parameterAdjustmentBox.setMaxWidth(width - 350);
         for (String param : params) {
             parameterAdjustmentBox.getItems().add(param);
         }
-        getChildren().addAll(background, xmlFilenameField, submitFileButton,
+        getChildren().addAll(background, xmlFileControl,
                 animationControl, parameterAdjustmentBox);
     }
 
     public String getXMLFilename() {
-        return xmlFilenameField.getCharacters().toString();
+        return xmlFileControl.getFilename();
     }
 
 
---
+++ src/gui/XMLFileControl.java
@@ -0,0 +1,36 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class XMLFileControl extends VBox {
+    private static final double PADDING = 25;
+
+    private TextField xmlFilenameField;
+    private Button submitFileButton;
+
+    public XMLFileControl(double xPos, double yPos, double width, double height,
+                          EventHandler<ActionEvent> submitFileHandler) {
+        super(PADDING);
+        setLayoutX(xPos);
+        setLayoutY(yPos);
+        setWidth(width);
+        setHeight(height);
+        setAlignment(Pos.CENTER);
+
+        xmlFilenameField = new TextField("XML GAME FILE");
+
+        submitFileButton = new Button("SUBMIT");
+        submitFileButton.setOnAction(submitFileHandler);
+
+        getChildren().addAll(xmlFilenameField, submitFileButton);
+    }
+
+    public String getFilename() {
+        return xmlFilenameField.getCharacters().toString();
+    }
+}
