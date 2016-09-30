133d869abc1d194f397a8d257f3d05a8523f3519
Will Long <wpl5@duke.edu>
2016-09-29 21:44:58 -0400

    Implemented percentage fills for initial locs

--- src/xmlparser/LocationParser.java
+++ src/xmlparser/LocationParser.java
@@ -1,13 +1,21 @@
 package xmlparser;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LocationParser implements Parser {
     private static final String ID_SECTION = "ID";
     private static final String ROW_SECTION = "ROW";
     private static final String COL_SECTION = "COL";
+    private static final String PERCENTAGE_SECTION = "PERCENT";
+    private static final String MANUAL_METHOD = "manual";
+    private static final String PERCENTAGE_METHOD = "percentage";
 
-    private int nextCellRow, nextCellCol, nextCellID;
+    private int nextCellRow, nextCellCol, nextCellID, nextCellPercent;
+    private int currentProbabilityPosition;
+    private List<Integer> probabilities;
 
     private boolean initialized;
     private List<Integer> initialCellTypeIDLocations;
@@ -16,12 +24,15 @@ public class LocationParser implements Parser {
 
     public LocationParser() {
         reset();
-
         initialized =false;
         initialCellTypeIDLocations = null;
+
         gridWidth = -1;
         gridHeight = -1;
         fillMethod = null;
+
+        currentProbabilityPosition = 0;
+        probabilities = null;
     }
 
     public void initializeGridInfo(List<Integer> cellTypeIDLocations, int gWidth, int gHeight, String method) {
@@ -30,6 +41,10 @@ public class LocationParser implements Parser {
         }
         initialized = true;
         initialCellTypeIDLocations = cellTypeIDLocations;
+        probabilities = new ArrayList<>();
+        for (int i = 0; i < cellTypeIDLocations.size(); i++) {
+            probabilities.add((int)(Math.random()*100));
+        }
         gridWidth = gWidth;
         gridHeight = gHeight;
         fillMethod = method;
@@ -40,16 +55,40 @@ public class LocationParser implements Parser {
         nextCellCol = -1;
         nextCellRow = -1;
         nextCellID = -1;
+        nextCellPercent = -1;
     }
 
     @Override
     public void update() {
+        if (fillMethod.equals(MANUAL_METHOD)) {
+            updateManual();
+        } else if (fillMethod.equals(PERCENTAGE_METHOD)) {
+            updatePercentage();
+        }
+    }
+
+    private void updateManual() {
         if (!initialized || nextCellRow == -1 || nextCellCol == -1 || nextCellID == -1) {
             return;
         }
         int pos = gridWidth * nextCellRow + nextCellCol;
         initialCellTypeIDLocations.remove(pos);
         initialCellTypeIDLocations.add(pos, nextCellID);
+    }
+
+    private void updatePercentage() {
+        if (!initialized || nextCellPercent == -1 || nextCellID == -1) {
+            return;
+        }
+        int nextProbabilityPosition = currentProbabilityPosition + nextCellPercent;
+        for (int i = 0; i < initialCellTypeIDLocations.size(); i++) {
+            int prob = probabilities.get(i);
+            if (prob >= currentProbabilityPosition && prob < nextProbabilityPosition) {
+                initialCellTypeIDLocations.remove(i);
+                initialCellTypeIDLocations.add(i, nextCellID);
+            }
+        }
+        currentProbabilityPosition = nextProbabilityPosition;
     }
 
     @Override
@@ -60,6 +99,8 @@ public class LocationParser implements Parser {
             nextCellRow = Integer.parseInt(infoValue);
         } else if (infoName.equals(COL_SECTION)) {
             nextCellCol = Integer.parseInt(infoValue);
+        } else if (infoName.equals(PERCENTAGE_SECTION)) {
+            nextCellPercent = Integer.parseInt(infoValue);
         }
     }
 }
