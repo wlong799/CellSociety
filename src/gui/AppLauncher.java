6779fa79cd9067ed889b31a7954f2ec29ad3bad3
Will Long <wpl5@duke.edu>
2016-09-30 15:35:27 -0400

    Refactored resources to be an enum

--- src/gui/AppLauncher.java
+++ src/gui/AppLauncher.java
@@ -4,24 +4,17 @@ import cellsociety_team13.AppResources;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.util.Properties;
-
 /**
  * AppLauncher class for CellSociety. Basic boilerplate code for launching the JavaFX
  * application.
  */
 public class AppLauncher extends Application {
-    private static final String RESOURCE_FILENAME = "Main";
-    private static final String APP_TITLE_KEY = "appTitle";
-    private Properties properties;
-
     private CellSocietyGUI cellSocietyGUI;
 
     @Override
     public void start (Stage stage) {
         cellSocietyGUI = new CellSocietyGUI();
-        properties = AppResources.getResourceFile(RESOURCE_FILENAME);
-        stage.setTitle(properties.getProperty(APP_TITLE_KEY));
+        stage.setTitle(AppResources.APP_TITLE.getResource());
         stage.setScene(cellSocietyGUI.getScene());
         stage.show();
     }
