ac21234b56a1280d37a61b6830ee2403758bea7b
Will Long <wpl5@duke.edu>
2016-10-02 17:03:52 -0400

    Finished Title Screen

--- src/gui/TitleScreen.java
+++ src/gui/TitleScreen.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -13,6 +14,8 @@ import java.io.*;
 public class TitleScreen extends Group {
     private ImageView backgroundImage;
     private Button startButton;
+    private ComboBox<String> fileSelect;
+    private String selectedFilename;
 
     public TitleScreen(double width, double height, EventHandler<ActionEvent> startButtonHandler) {
         backgroundImage = new ImageView();
@@ -34,7 +37,28 @@ public class TitleScreen extends Group {
         double buttonY = height - AppResources.TITLE_BUTTON_OFFSET.getDoubleResource();
         startButton.setLayoutX(buttonX);
         startButton.setLayoutY(buttonY);
-        getChildren().addAll(backgroundImage, startButton);
+
+        fileSelect = new ComboBox<>();
+        fileSelect.setPrefWidth(AppResources.TITLE_FILESELECT_WIDTH.getDoubleResource());
+        double fileSelectX = (width / 2) - (fileSelect.getPrefWidth() / 2);
+        double fileSelectY = height - AppResources.TITLE_FILESELECT_OFFSET.getDoubleResource();
+        fileSelect.setLayoutX(fileSelectX);
+        fileSelect.setLayoutY(fileSelectY);
+        File dataDirectory = new File(AppResources.APP_DATA.getResource());
+        File[] dataFiles = dataDirectory.listFiles();
+        for (File file : dataFiles) {
+            fileSelect.getItems().add(file.getName());
+        }
+
+        selectedFilename = null;
+        fileSelect.valueProperty().addListener((observable, oldValue, newValue) -> {
+                    selectedFilename = newValue;
+                });
+
+        getChildren().addAll(backgroundImage, startButton, fileSelect);
     }
 
+    public String getXMLFilename() {
+        return selectedFilename;
+    }
 }
